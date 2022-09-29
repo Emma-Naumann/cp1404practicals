731135832563439dbdbe96248877aabe63ea6e3b
emmanaumann <emma.naumann@my.jcu.edu.au>
2022-09-29 16:02:44 +1000

    Update string formatting exercise

---
+++ prac_03/randoms.py
@@ -0,0 +1,27 @@
+"""
+Emma Naumann
+CP1404 Practical 3
+Random Numbers
+"""
+
+
+# print(random.randint(5, 20))  # line 1
+# 10
+# 19
+# 15
+# The smallest: 5
+# The largest: 20
+# This is because randint is inclusive of endpoints
+
+
+# print(random.randrange(3, 10, 2))  # line 2
+# 7
+# 3
+# 7
+# Smallest: 3
+# Largest: 9
+# This is because randrange is not inclusive of endpoint values
+# Line 2 could NOT produce a 4 since it is in steps of 2
+
+
+# print(random.uniform(2.5, 5.5))  # line 3

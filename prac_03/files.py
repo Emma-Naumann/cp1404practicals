516cb33a40522032a8d71d63b2093e7e169e4da8
emmanaumann <emma.naumann@my.jcu.edu.au>
2022-10-01 19:39:58 +1000

    Add file to prac_03 folder

---
+++ prac_03/files.py
@@ -0,0 +1,33 @@
+"""
+Emma Naumann
+CP1404 Practical 3
+Files
+"""
+
+# # Question 1
+# name = input("Name: ")
+# with open("name.txt", "w") as out_file:
+#     print(name, file=out_file)
+#
+#
+# # Question 2
+# in_file = open("name.txt")
+# name = in_file.read().strip()
+# in_file.close()
+# print("Your name is", name)
+
+# Question 3
+in_file = open("numbers.txt")
+number_1 = int(in_file.readline())
+number_2 = int(in_file.readline())
+in_file.close()
+print(number_1 + number_2)
+
+# Question 4
+in_file = open("numbers.txt")
+total = 0
+for line in in_file:
+    number = int(line)
+    total += number
+in_file.close()
+print(total)

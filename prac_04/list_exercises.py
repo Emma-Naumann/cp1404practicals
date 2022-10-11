dee2c2e4152ae289e29c6f06e211cedcdfbdda78
emmanaumann <emma.naumann@my.jcu.edu.au>
2022-10-11 11:32:13 +1000

    Add list_exercises.py to prac_04 folder

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,40 @@
+"""
+Emma Naumann
+CP1404 Practical 4
+Basic List Operations and Woefully Inadequate Security Checker
+"""
+
+# # Basic list operations
+# def main():
+#     """Get numbers from user and display input."""
+#     numbers = get_numbers()
+#     display_numbers(numbers)
+#
+#
+# def get_numbers():
+#     numbers = []
+#     for i in range(5):
+#         number = int(input("Number: "))
+#         numbers.append(number)
+#     return numbers
+#
+#
+# def display_numbers(numbers):
+#     print(f"The first number is {numbers[0]}")
+#     print(f"The last number is {numbers[-1]}")
+#     print(f"The smallest number is {min(numbers)}")
+#     print(f"The largest number is {max(numbers)}")
+#     print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+#
+#
+# main()
+
+
+# Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access granted.")
+else:
+    print("Access denied.")

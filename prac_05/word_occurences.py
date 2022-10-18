be7518d68cbe870942fc992942f8c63666525c87
emmanaumann <emma.naumann@my.jcu.edu.au>
2022-10-18 11:47:20 +1000

    Add file to prac_05 folder

---
+++ prac_05/word_occurences.py
@@ -0,0 +1,21 @@
+"""
+Emma Naumann
+CP1404 Practical 5
+
+Word occurences
+Estimate: 30 minutes
+Actual: 15 minutes
+"""
+
+text = input("Text: ")
+words = text.split(" ")
+word_to_count = {}
+for word in words:
+    try:
+        word_to_count[word] += 1
+    except KeyError:
+        word_to_count[word] = 1
+max_length = max(len(word) for word in word_to_count.keys())
+for word, count in sorted(word_to_count.items()):
+    print(f"{word:{max_length}} : {count}")
+

fff4d3ad9daa321270269a9bc7b61e3757711865
emmanaumann <emma.naumann@my.jcu.edu.au>
2022-11-09 00:48:38 +1000

    Display formatted guitars list

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -38,7 +38,7 @@ def display_guitars(guitars):
     print("These are my guitars:")
     for i, guitar in enumerate(guitars, 1):
         vintage_string = " (vintage)" if guitar.is_vintage() else ""
-        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+        print(f"Guitar {i}: {guitar.name:30} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
 
 
 def add_guitars(guitars):

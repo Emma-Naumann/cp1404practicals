aa67e8a9c27599ce743bfc6811bf2f2ae7217873
emmanaumann <emma.naumann@my.jcu.edu.au>
2022-11-23 16:08:48 +1000

    Add file to prac_09 folder

---
+++ prac_09/band.py
@@ -0,0 +1,33 @@
+from prac_09.musician import Musician
+
+
+class Band(Musician):
+    """Band class."""
+
+    def __init__(self, band_name="", **kwargs):
+        """Initialise a Band."""
+        super().__init__(**kwargs)
+        self.band_name = band_name
+        self.musicians = []
+
+    def __str__(self):
+        """Return a string representation of a Band."""
+        return f"{self.band_name} ({[musician for musician in self.musicians]})"
+        # return f"{self.band_name}"
+
+
+    def __repr__(self):
+        """Return a string representation of a Musician, showing the variables."""
+        return str(vars(self))
+
+    def add(self, musician):
+        """Add a musician to band."""
+        self.musicians.append(musician)
+    #
+    # def play(self):
+    #     """Return a string showing the musician playing their first (or no) instrument."""
+    #
+    #     if not self.instruments:
+    #         return f"{self.musician_name} needs an instrument!"
+    #     return f"{self.musician_name} is playing: {self.instruments[0]}"
+

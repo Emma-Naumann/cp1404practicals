9f48c68d9433000336649b0d03cd0ec618c98498
emmanaumann <emma.naumann@my.jcu.edu.au>
2022-11-30 09:40:30 +1000

    Fix spelling mistake

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -6,7 +6,7 @@ Wikipedia search and summary using user input program
 
 import wikipedia
 
-search_phrase = input("Ssearch phrase: ")
+search_phrase = input("Search phrase: ")
 while search_phrase != "":
     try:
         # print(wikipedia.summary(search_phrase))
